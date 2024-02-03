5e6df383d31d1a9cf1d744e931a7c2bda67f7490
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2024-02-03 23:06:01 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.header("Welcome!", divider="rainbow")
+
+st.write(
+    """
+    I humbly welcome you to my portfolio website. Although there is no content yet, but it's currently WIP _(Really!)_. 
+    My first thoughts was to write about how I created this website using AWS services because it had been a great learning
+    experience for me! But more on that soon.. 😎
+    """
+)
